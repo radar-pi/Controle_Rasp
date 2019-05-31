b56ee91a28e7155c24b68792a249b656218ce1d7
Rodrigo Sousa <rodrigo.sousa2711@gmail.com>
2019-05-30 23:21:24 -0300

    Adiciona código NRF de transmissão e recepção

--- NRF24/testesendrecv.py
+++ NRF24/testesendrecv.py
@@ -17,94 +17,83 @@ import sys
 
 pipes = [[0xe7, 0xe7, 0xe7, 0xe7, 0xe7], [0xc2, 0xc2, 0xc2, 0xc2, 0xc2]]
 
-radio = NRF24(GPIO, spidev.SpiDev())
-radio.begin(0, 17)
-time.sleep(1)
-radio.setRetries(15,15)
-radio.setPayloadSize(32)
-radio.setChannel(0x60)
 
-radio.setDataRate(NRF24.BR_2MBPS)
-radio.setPALevel(NRF24.PA_MIN)
-radio.setAutoAck(True)
-radio.enableDynamicPayloads()
-radio.enableAckPayload()
+radio_tx = NRF24(GPIO, spidev.SpiDev())
+radio_tx.begin(0, 17)
+radio_tx.setRetries(15,15)
+radio_tx.setPayloadSize(32)
+radio_tx.setChannel(0x60)
 
+radio_tx.setDataRate(NRF24.BR_2MBPS)
+radio_tx.setPALevel(NRF24.PA_MIN)
+radio_tx.setAutoAck(True)
+radio_tx.enableDynamicPayloads()
+radio_tx.enableAckPayload()
 
-radio.openWritingPipe(pipes[1])
-radio.openReadingPipe(1, pipes[0])
-radio.printDetails()
+radio_tx.openWritingPipe(pipes[1])
+radio_tx.openReadingPipe(1, pipes[0])
+radio_tx.printDetails()
 
-if not radio.isPVariant():
-       radio.printDetails()
-       print ("NRF24L01+ not found.")
-       exit()
+radio_rx = NRF24(GPIO, spidev.SpiDev())
+radio_rx.begin(0, 17)
+
+radio_rx.setRetries(15,15)
+
+radio_rx.setPayloadSize(32)
+radio_rx.setChannel(0x60)
+radio_rx.setDataRate(NRF24.BR_2MBPS)
+radio_rx.setPALevel(NRF24.PA_MIN)
+
+radio_rx.setAutoAck(True)
+radio_rx.enableDynamicPayloads()
+radio_rx.enableAckPayload()
+
+radio_rx.openWritingPipe(pipes[0])
+radio_rx.openReadingPipe(1, pipes[1])
+
+radio_rx.startListening()
+radio_rx.stopListening()
+
+radio_rx.printDetails()
+
+radio_rx.startListening()
 
 def envio():
-    while True:
-        buf = [1]
-        radio.write(buf)
-        print ("Enviado:", buf),
-        if radio.isAckPayloadAvailable():
-            pl_buffer=[]
-            radio.read(pl_buffer, radio.getDynamicPayloadSize())
-            print ("Retorno:", pl_buffer)
-        else:
-            print ("Sem conexão envio: 0")
-        time.sleep(1)
- 
- 
-radio2 = NRF24(GPIO, spidev.SpiDev())
-radio2.begin(0, 17)
 
-radio2.setRetries(15,15)
 
-radio2.setPayloadSize(32)
-radio2.setChannel(0x60)
-radio2.setDataRate(NRF24.BR_2MBPS)
-radio2.setPALevel(NRF24.PA_MIN)
+    buf = [1]
+    radio_tx.write(buf)
+    print ("Enviado:", buf),
+    if radio_tx.isAckPayloadAvailable():
+        pl_buffer=[]
+        radio_tx.read(pl_buffer, radio_tx.getDynamicPayloadSize())
+        print ("Retorno:", pl_buffer)
+    else:
+        print ("Sem conexão envio: 0")
 
-radio2.setAutoAck(True)
-radio2.enableDynamicPayloads()
-radio2.enableAckPayload()
-
-radio2.openWritingPipe(pipes[0])
-radio2.openReadingPipe(1, pipes[1])
-
-radio2.startListening()
-radio2.stopListening()
-
-radio2.printDetails()
-
-radio2.startListening()
-
-c=1
-r=1
 def recebe():
-    while True:
+    c=1
+    r=1
+   
 	
-        akpl_buf = [r]
-        pipe = [0]
-        while not radio2.available(pipe):
-            time.sleep(10000/1000000.0)
+    akpl_buf = [r]
+    pipe = [0]
+    while not radio_rx.available(pipe):
+        time.sleep(10000/1000000.0)
 
-        recv_buffer = []
-        radio2.read(recv_buffer, radio2.getDynamicPayloadSize())
-        print ("Recebido:", recv_buffer)
-        c = c + 1
-        if (c&1) == 0:
-            radio2.writeAckPayload(1, akpl_buf, len(akpl_buf))
-            print ("Retorna:", akpl_buf)
-            r = r+1
-        else:
-            print ("Sem conexão: 0")
-        time.sleep(1)
- 
-
-
+    recv_buffer = []
+    radio_rx.read(recv_buffer, radio_rx.getDynamicPayloadSize())
+    print ("Recebido:", recv_buffer)
+    c = c + 1
+    if (c&1) == 0:
+        radio_rx.writeAckPayload(1, akpl_buf, len(akpl_buf))
+        print ("Retorna:", akpl_buf)
+        r = r+1
+    else:
+        print ("Sem conexão: 0")
 
 while True:
-        envio()   # send something
-        time.sleep(1)
-        recebe()    # has it arrived? (if so, maybe send return data)
-        time.sleep(1)   # 1 sec per loop
+    envio()   # send something
+    time.sleep(1)
+    recebe()    # has it arrived? (if so, maybe send return data)
+    time.sleep(1)   # 1 sec per loop
